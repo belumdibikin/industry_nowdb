e71fc040cbfdf3992697c8eec6288537a98b8591
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2014-08-13 11:48:11 +0700

    New 1. New Home Page

--- asset/style/js/main.js
+++ asset/style/js/main.js
@@ -297,12 +297,14 @@ var webApp = function() {
 
     /* Datatables Bootstrap integration */
     var dtIntegration = function() {
+
+        // Set the defaults for DataTables initialization
         $.extend(true, $.fn.dataTable.defaults, {
             "sDom": "<'row'<'col-sm-6 col-xs-5'l><'col-sm-6 col-xs-7'f>r>t<'row'<'col-sm-5 hidden-xs'i><'col-sm-7 col-xs-12 clearfix'p>>",
             "sPaginationType": "bootstrap",
             "oLanguage": {
                 "sLengthMenu": "_MENU_",
-                "sSearch": "<div class=\"input-group\">_INPUT_<span class=\"input-group-addon\"><i class=\"fa fa-search\"></i></span></div>",
+                "sSearch": "<div class=\"input-group\"><span class=\"input-group-addon\"><i class=\"icon-search\"></i></span>_INPUT_</div>",
                 "sInfo": "<strong>_START_</strong>-<strong>_END_</strong> of <strong>_TOTAL_</strong>",
                 "oPaginate": {
                     "sPrevious": "",
@@ -310,10 +312,100 @@ var webApp = function() {
                 }
             }
         });
+
+        // Default class modification
         $.extend($.fn.dataTableExt.oStdClasses, {
-            "sWrapper": "dataTables_wrapper form-inline",
-            "sFilterInput": "form-control",
-            "sLengthSelect": "form-control"
+            "sWrapper": "dataTables_wrapper form-inline"
+        });
+
+        // API method to get paging information
+        $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
+        {
+            return {
+                "iStart": oSettings._iDisplayStart,
+                "iEnd": oSettings.fnDisplayEnd(),
+                "iLength": oSettings._iDisplayLength,
+                "iTotal": oSettings.fnRecordsTotal(),
+                "iFilteredTotal": oSettings.fnRecordsDisplay(),
+                "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+                "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+            };
+        };
+
+        // Bootstrap style pagination control
+        $.extend($.fn.dataTableExt.oPagination, {
+            "bootstrap": {
+                "fnInit": function(oSettings, nPaging, fnDraw) {
+                    var oLang = oSettings.oLanguage.oPaginate;
+                    var fnClickHandler = function(e) {
+                        e.preventDefault();
+                        if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
+                            fnDraw(oSettings);
+                        }
+                    };
+
+                    $(nPaging).append(
+                        '<ul class="pagination pagination-sm remove-margin">' +
+                        '<li class="prev disabled"><a href="javascript:void(0)"><i class="icon-chevron-left"></i> ' + oLang.sPrevious + '</a></li>' +
+                        '<li class="next disabled"><a href="javascript:void(0)">' + oLang.sNext + ' <i class="icon-chevron-right"></i></a></li>' +
+                        '</ul>'
+                        );
+                    var els = $('a', nPaging);
+                    $(els[0]).bind('click.DT', {action: "previous"}, fnClickHandler);
+                    $(els[1]).bind('click.DT', {action: "next"}, fnClickHandler);
+                },
+                "fnUpdate": function(oSettings, fnDraw) {
+                    var iListLength = 5;
+                    var oPaging = oSettings.oInstance.fnPagingInfo();
+                    var an = oSettings.aanFeatures.p;
+                    var i, j, sClass, iStart, iEnd, iHalf = Math.floor(iListLength / 2);
+
+                    if (oPaging.iTotalPages < iListLength) {
+                        iStart = 1;
+                        iEnd = oPaging.iTotalPages;
+                    }
+                    else if (oPaging.iPage <= iHalf) {
+                        iStart = 1;
+                        iEnd = iListLength;
+                    } else if (oPaging.iPage >= (oPaging.iTotalPages - iHalf)) {
+                        iStart = oPaging.iTotalPages - iListLength + 1;
+                        iEnd = oPaging.iTotalPages;
+                    } else {
+                        iStart = oPaging.iPage - iHalf + 1;
+                        iEnd = iStart + iListLength - 1;
+                    }
+
+                    for (i = 0, iLen = an.length; i < iLen; i++) {
+                        // Remove the middle elements
+                        $('li:gt(0)', an[i]).filter(':not(:last)').remove();
+
+                        // Add the new list items and their event handlers
+                        for (j = iStart; j <= iEnd; j++) {
+                            sClass = (j === oPaging.iPage + 1) ? 'class="active"' : '';
+                            $('<li ' + sClass + '><a href="javascript:void(0)">' + j + '</a></li>')
+                                .insertBefore($('li:last', an[i])[0])
+                                .bind('click', function(e) {
+                                e.preventDefault();
+                                oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
+                                fnDraw(oSettings);
+                            });
+                        }
+
+                        // Add / remove disabled classes from the static elements
+                        if (oPaging.iPage === 0) {
+                            $('li:first', an[i]).addClass('disabled');
+                        } else {
+                            $('li:first', an[i]).removeClass('disabled');
+                        }
+
+                        if (oPaging.iPage === oPaging.iTotalPages - 1 || oPaging.iTotalPages === 0) {
+                            $('li:last', an[i]).addClass('disabled');
+                        } else {
+                            $('li:last', an[i]).removeClass('disabled');
+                        }
+                    }
+                }
+            }
         });
     };
 
