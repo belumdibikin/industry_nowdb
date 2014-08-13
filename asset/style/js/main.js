69da3cafbbbfa57c1c9d858bb17cdcfb022c6b6a
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2014-08-13 14:15:48 +0700

    New 1. Fix Data Table

--- asset/style/js/main.js
+++ asset/style/js/main.js
@@ -11,22 +11,26 @@
 var webApp = function() {
 
     /* Initialization UI Code */
-    var uiInit = function () {
+    var uiInit = function() {
 
         // Add the  correct copyright year at the footer
         var yearCopy = $('#year-copy'), d = new Date();
 
-        if (d.getFullYear() === 2013) { yearCopy.html('2013'); } else { yearCopy.html('2013-' + d.getFullYear().toString().substr(2,2)); }
+        if (d.getFullYear() === 2013) {
+            yearCopy.html('2013');
+        } else {
+            yearCopy.html('2013-' + d.getFullYear().toString().substr(2, 2));
+        }
 
         // Set min-height to #page-content, so that footer is visible at the bottom if there is not enough content
         var pageContent = $('#page-content');
 
         pageContent.css('min-height', $(window).height() -
-            ($('header').height() + $('footer').outerHeight()) + 'px');
+                ($('header').height() + $('footer').outerHeight()) + 'px');
 
         $(window).resize(function() {
             pageContent.css('min-height', $(window).height() -
-                ($('header').height() + $('footer').outerHeight()) + 'px');
+                    ($('header').height() + $('footer').outerHeight()) + 'px');
         });
 
         // If .navbar-fixed class added to <header>, add padding to the page-container
@@ -38,30 +42,39 @@ var webApp = function() {
 
         // Select/Deselect all checkboxes in tables
         $('thead input:checkbox').click(function() {
-            var checkedStatus   = $(this).prop('checked');
-            var table           = $(this).closest('table');
+            var checkedStatus = $(this).prop('checked');
+            var table = $(this).closest('table');
 
-            $('tbody input:checkbox', table).each(function() { $(this).prop('checked', checkedStatus); });
+            $('tbody input:checkbox', table).each(function() {
+                $(this).prop('checked', checkedStatus);
+            });
         });
 
         // Initialize tabs
-        $('[data-toggle="tabs"] a').click(function (e) { e.preventDefault(); $(this).tab('show'); });
+        $('[data-toggle="tabs"] a').click(function(e) {
+            e.preventDefault();
+            $(this).tab('show');
+        });
 
         // Gallery hover options functionality
         $('[data-toggle="gallery-options"] > li')
-           .mouseover(function(){ $(this).find('.thumbnails-options').show(); })
-           .mouseout(function(){ $(this).find('.thumbnails-options').hide();});
+                .mouseover(function() {
+                    $(this).find('.thumbnails-options').show();
+                })
+                .mouseout(function() {
+                    $(this).find('.thumbnails-options').hide();
+                });
 
         // Initialize Slimscroll
-        $('.scrollable').slimScroll({ height: '100px', size: '3px', touchScrollStep: 100 });
-        $('.scrollable-tile').slimScroll({ height: '130px', size: '3px', touchScrollStep: 100 });
-        $('.scrollable-tile-2x').slimScroll({ height: '330px', size: '3px', touchScrollStep: 100 });
+        $('.scrollable').slimScroll({height: '100px', size: '3px', touchScrollStep: 100});
+        $('.scrollable-tile').slimScroll({height: '130px', size: '3px', touchScrollStep: 100});
+        $('.scrollable-tile-2x').slimScroll({height: '330px', size: '3px', touchScrollStep: 100});
 
         // Initialize Tooltips
-        $('[data-toggle="tooltip"]').tooltip({ container: 'body', animation: false });
+        $('[data-toggle="tooltip"]').tooltip({container: 'body', animation: false});
 
         // Initialize Popovers
-        $('[data-toggle="popover"]').popover({ container: 'body', animation: false });
+        $('[data-toggle="popover"]').popover({container: 'body', animation: false});
 
         // Initialize Chosen
         $('.select-chosen').chosen();
@@ -92,10 +105,10 @@ var webApp = function() {
     };
 
     /* Demo Code */
-    var uiDemo = function () {
+    var uiDemo = function() {
 
         // Demo of how loading of widgets could happen
-        $('.loading-on').click(function(){
+        $('.loading-on').click(function() {
 
             // Get loading div from header
             var loading = $('#loading');
@@ -107,7 +120,7 @@ var webApp = function() {
             $('#widgets > li > a > .badge').addClass('display-none');
 
             // Wait a while (the real updating could happen here)
-            setTimeout(function(){
+            setTimeout(function() {
                 // Fade Out loading and add some demo badges
                 loading.fadeOut(250);
 
@@ -117,9 +130,9 @@ var webApp = function() {
         });
 
         // Initialize Typeahead - Example with countries
-        var data = ["Afghanistan","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Côte d'Ivoire","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Cook Islands","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Democratic Republic of the Congo","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Faeroe Islands","Falkland Islands","Fiji","Finland","Former Yugoslav Republic of Macedonia","France","French Guiana","French Polynesia","French Southern Territories","Gabon","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and McDonald Islands","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","North Korea","Northern Marianas","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn Islands","Poland","Portugal","Puerto Rico","Qatar","Réunion","Romania","Russia","Rwanda","São Tomé and Príncipe","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Saudi Arabia","Senegal","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","South Korea","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","The Bahamas","The Gambia","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","US Virgin Islands","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Wallis and Futuna","Western Sahara","Yemen","Yugoslavia","Zambia","Zimbabwe"];
+        var data = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Côte d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland", "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar", "Réunion", "Romania", "Russia", "Rwanda", "São Tomé and Príncipe", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas", "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "US Virgin Islands", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"];
 
-        $('.example-typeahead').typeahead({ items: 5, source: data });
+        $('.example-typeahead').typeahead({items: 5, source: data});
 
         // Initialize DateRangePicker Advanced Demo Example
         var exampleAdvancedDateRange = $('#example-advanced-daterangepicker');
@@ -144,18 +157,20 @@ var webApp = function() {
     };
 
     /* Primary navigation functionality */
-    var primaryNav = function () {
+    var primaryNav = function() {
 
         // Get all primary navigation top links
         var menuLinks = $('#primary-nav > ul > li > a');
 
         // Initialize submenu number indicators
-        menuLinks.filter(function(){ return $(this).next().is('ul'); }).each(function(n, e){
+        menuLinks.filter(function() {
+            return $(this).next().is('ul');
+        }).each(function(n, e) {
             $(e).append('<span>' + $(e).next('ul').children().length + '</span>');
         });
 
         // Accordion functionality
-        menuLinks.click(function(){
+        menuLinks.click(function() {
 
             var link = $(this);
 
@@ -183,7 +198,7 @@ var webApp = function() {
         // Get link
         var link = $('#to-top');
 
-        $(window).scroll(function(){
+        $(window).scroll(function() {
 
             // If the user scrolled a bit (150 pixels) show the link
             if ($(this).scrollTop() > 150) {
@@ -194,40 +209,46 @@ var webApp = function() {
         });
 
         // On click get to top
-        link.click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 300);
+        link.click(function() {
+            $("html, body").animate({scrollTop: 0}, 300);
             return false;
         });
     };
 
     /* Theme Options, change features */
-    var themeOptions = function () {
+    var themeOptions = function() {
 
         // Get all switches
-        var toptFixedHeaderTop      = $('#topt-fixed-header-top input');
-        var toptFixedHeaderBottom   = $('#topt-fixed-header-bottom input');
-        var toptFixedLayout         = $('#topt-fixed-layout input');
+        var toptFixedHeaderTop = $('#topt-fixed-header-top input');
+        var toptFixedHeaderBottom = $('#topt-fixed-header-bottom input');
+        var toptFixedLayout = $('#topt-fixed-layout input');
 
         // Cache elements
-        var header  = $('header');
+        var header = $('header');
         var pageCon = $('#page-container');
-        var body    = $('body');
+        var body = $('body');
 
         // Show-Hide Theme Options
-        $('.btn-theme-options').click(function(){
+        $('.btn-theme-options').click(function() {
             $(this).toggleClass('open');
             $('#theme-options-content').slideToggle(200);
             return false;
         });
 
         // Initialize checkboxes
-        if (header.hasClass('navbar-fixed-top')){ toptFixedHeaderTop.prop('checked', true); }
-        if (header.hasClass('navbar-fixed-bottom')){ toptFixedHeaderBottom.prop('checked', true); }
-        if (body.hasClass('fixed')){ toptFixedLayout.prop('checked', true); }
+        if (header.hasClass('navbar-fixed-top')) {
+            toptFixedHeaderTop.prop('checked', true);
+        }
+        if (header.hasClass('navbar-fixed-bottom')) {
+            toptFixedHeaderBottom.prop('checked', true);
+        }
+        if (body.hasClass('fixed')) {
+            toptFixedLayout.prop('checked', true);
+        }
 
         // Change header to fixed top
         toptFixedHeaderTop.change(function() {
-            if($(this).is(":checked")) {
+            if ($(this).is(":checked")) {
                 toptFixedHeaderBottom.prop('checked', false);
                 header.removeClass('navbar-fixed-bottom').addClass('navbar-fixed-top');
                 pageCon.removeClass('header-fixed-bottom').addClass('header-fixed-top');
@@ -239,7 +260,7 @@ var webApp = function() {
 
         // Change header to fixed bottom
         toptFixedHeaderBottom.change(function() {
-            if($(this).is(":checked")) {
+            if ($(this).is(":checked")) {
                 toptFixedHeaderTop.prop('checked', false);
                 header.removeClass('navbar-fixed-top').addClass('navbar-fixed-bottom');
                 pageCon.removeClass('header-fixed-top').addClass('header-fixed-bottom');
@@ -251,7 +272,7 @@ var webApp = function() {
 
         // Change layout to fixed
         toptFixedLayout.change(function() {
-            if($(this).is(":checked")) {
+            if ($(this).is(":checked")) {
                 body.addClass('fixed');
             } else {
                 body.removeClass('fixed');
@@ -272,7 +293,7 @@ var webApp = function() {
             $('a[data-theme="' + theme + '"]', colorList).parent('li').addClass('active');
         }
 
-        $('a', colorList).click(function(e){
+        $('a', colorList).click(function(e) {
             // Get theme name
             theme = $(this).data('theme');
 
@@ -304,7 +325,7 @@ var webApp = function() {
             "sPaginationType": "bootstrap",
             "oLanguage": {
                 "sLengthMenu": "_MENU_",
-                "sSearch": "<div class=\"input-group\"><span class=\"input-group-addon\"><i class=\"icon-search\"></i></span>_INPUT_</div>",
+                "sSearch": "<div class=\"input-group\"><span class=\"input-group-addon\"><i class=\"fa fa-search\"></i></span>_INPUT_</div>",
                 "sInfo": "<strong>_START_</strong>-<strong>_END_</strong> of <strong>_TOTAL_</strong>",
                 "oPaginate": {
                     "sPrevious": "",
@@ -345,11 +366,11 @@ var webApp = function() {
                     };
 
                     $(nPaging).append(
-                        '<ul class="pagination pagination-sm remove-margin">' +
-                        '<li class="prev disabled"><a href="javascript:void(0)"><i class="icon-chevron-left"></i> ' + oLang.sPrevious + '</a></li>' +
-                        '<li class="next disabled"><a href="javascript:void(0)">' + oLang.sNext + ' <i class="icon-chevron-right"></i></a></li>' +
-                        '</ul>'
-                        );
+                            '<ul class="pagination pagination-sm remove-margin">' +
+                            '<li class="prev disabled"><a href="javascript:void(0)"><i class="fa fa-chevron-circle-left"></i> ' + oLang.sPrevious + '</a></li>' +
+                            '<li class="next disabled"><a href="javascript:void(0)">' + oLang.sNext + ' <i class="fa fa-chevron-circle-right"></i></a></li>' +
+                            '</ul>'
+                            );
                     var els = $('a', nPaging);
                     $(els[0]).bind('click.DT', {action: "previous"}, fnClickHandler);
                     $(els[1]).bind('click.DT', {action: "next"}, fnClickHandler);
@@ -383,12 +404,12 @@ var webApp = function() {
                         for (j = iStart; j <= iEnd; j++) {
                             sClass = (j === oPaging.iPage + 1) ? 'class="active"' : '';
                             $('<li ' + sClass + '><a href="javascript:void(0)">' + j + '</a></li>')
-                                .insertBefore($('li:last', an[i])[0])
-                                .bind('click', function(e) {
-                                e.preventDefault();
-                                oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
-                                fnDraw(oSettings);
-                            });
+                                    .insertBefore($('li:last', an[i])[0])
+                                    .bind('click', function(e) {
+                                        e.preventDefault();
+                                        oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
+                                        fnDraw(oSettings);
+                                    });
                         }
 
                         // Add / remove disabled classes from the static elements
@@ -410,8 +431,7 @@ var webApp = function() {
     };
 
     return {
-
-        init: function () {
+        init: function() {
             uiInit(); // Initialize UI Code
             uiDemo(); // Initialize Demo Code
             primaryNav(); // Primary Navigation functionality
@@ -423,4 +443,6 @@ var webApp = function() {
 }();
 
 /* Initialize WebApp when page loads */
-$(function(){ webApp.init(); });
+$(function() {
+    webApp.init();
+});
